4690554376abc1d1b9420b62cf603d4f2a6dcde7
Gakelorm <111369887+Gakelorm@users.noreply.github.com>
2022-09-28 19:21:32 -0400

    Update sonar-project.js

--- sonar-project.js
+++ sonar-project.js
@@ -6,7 +6,7 @@ sonarqubeScanner({
 	    'sonar.projectDescription': 'This is a Node JS application',
 	    'sonar.projectName': 'Node JS Application - Sample',
 	    'sonar.projectKey':'NodeJsLandmarkTechnologies',
-	    'sonar.login': '6536bd2351f6e04713ace5d9b63babea31f34951',
+	    'sonar.login': 'e3f272562b5ab13216a0a9d48d58a8639aa603ad',
 	    //'sonar.password': 'admin',
             'sonar.projectVersion':'1.0',
 	    'sonar.language':'js',
